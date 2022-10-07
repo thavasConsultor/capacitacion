cb66d17cbd7bdd28d18044525a72a81ebdec0e5e
malmengor <malmengor@thavas.xyz>
2022-10-07 10:07:16 -0500

    ingresado el manifest e init

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Thavas",
+
+    'summary': "Capacitacion",
+
+    'description': "codigo de  prueba para verificar si todo esta bien.",
+
+    'author': "THAVAS",
+    'website': "http://www.thavasconsultoria.com/odoo",
+
+    'category': 'Retail',
+    'version': '15.0.1.3',
+
+    'depends': "",
+
+    'data': "",
+
+    'demostration': '',
+}
