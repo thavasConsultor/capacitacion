c74fd7dc45af2c3ce0c4215011ad79249df559cb
malmengor <malmengor@thavas.xyz>
2022-10-10 13:23:14 -0500

    probando cambios desde gitbash

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Thavas",
+    'name': "curso",
 
-    'summary': "Capacitacion",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
 
-    'description': "codigo de  prueba para verificar si todo esta bien.",
+    'description': """
+        Long description of module's purpose
+    """,
 
-    'author': "THAVAS",
-    'website': "http://www.thavasconsultoria.com/odoo",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
-    'category': 'Retail',
-    'version': '15.0.1.3',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
 
-    'depends': "",
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
 
-    'data': "",
-
-    'demostration': '',
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
 }
